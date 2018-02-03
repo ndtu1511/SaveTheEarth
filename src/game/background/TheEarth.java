5373d9c18f6ab3d02120b1433291385ff57cb2ca
VuDongBui <vudongbui@gmail.com>
2018-02-04 06:00:02 +0700

    Merge branch 'master' of https://github.com/ndtu1511/SaveTheEarth
    # Conflicts:
    #	src/GameCanvas.java

--- src/game/background/TheEarth.java
+++ src/game/background/TheEarth.java
@@ -6,7 +6,7 @@ import constants.Constant;
 
 public class TheEarth extends GameObject {
     public TheEarth() {
-        this.renderer = new ImageRenderer(Constant.TheEarth.PATH);
+        this.renderer = new ImageRenderer("resources/earth/earth.png");
         this.position.set(Constant.Windows.WIDTH/2,Constant.Windows.HEIGHT/2);
     }
 }
