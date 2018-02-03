a8c6e1eef6cda4c22153cf89abc6595c17b53ece
ndtu1511 <ndtu.1511@gmail.com>
2018-02-04 03:49:35 +0700

    Merge branch 'master' of https://github.com/ndtu1511/SaveTheEarth
    # Conflicts:
    #	src/GameCanvas.java

--- src/game/background/BackGround.java
+++ src/game/background/BackGround.java
@@ -2,10 +2,12 @@ package game.background;
 
 import Core.GameObject;
 import Renderer.ImageRenderer;
+import constants.Constant;
 import utils.Utils;
 
-public class Background extends GameObject {
-    public Background() {
+public class BackGround extends GameObject {
+    public BackGround() {
         this.renderer = new ImageRenderer("resources/background/background.jpg");
+        this.position.set(Constant.Windows.WIDTH/2,Constant.Windows.HEIGHT/2);
     }
 }
