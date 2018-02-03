43ba835184576e3f7a2c00c8df6d2574be133013
ndtu1511 <ndtu.1511@gmail.com>
2018-02-04 00:50:56 +0700

    created project

---
+++ src/game/background/BackGround.java
@@ -0,0 +1,13 @@
+package game.background;
+
+import Core.GameObject;
+import Renderer.ImageRenderer;
+import constants.Constant;
+
+public class BackGround extends GameObject {
+    public BackGround(){
+        this.renderer = new ImageRenderer(Constant.BackGround.PATH);
+        this.position.set(Constant.Windows.WIDTH/2,Constant.Windows.HEIGHT/2);
+
+    }
+}
