3d1f6a42cb24bef6b2a942dc5d03abffc4df850d
Tran Dat <tranquangdat1397@gmail.com>
2018-02-09 22:34:33 +0700

    add sound

--- src/game/meteorite/RocketHit.java
+++ src/game/meteorite/RocketHit.java
@@ -2,15 +2,21 @@ package game.meteorite;
 
 import game.canon.Rocket;
 import hit.RunHitObject;
+import utils.AudioUtils;
+
+import javax.sound.sampled.Clip;
 
 public class RocketHit {
     private RunHitObject[] arrays;
+    private Clip clip;
 
     public RocketHit() {
         this.arrays = new RunHitObject[]{
                 new RunHitObject(Meteorite.class),
 //                new RunHitObject(SquareBullet.class)
         };
+        this.clip = AudioUtils.instance.loadSound("resources/Sound/sfx/explosive.wav");
+        this.clip.start();
     }
 
     public void run(Rocket rocket) {
